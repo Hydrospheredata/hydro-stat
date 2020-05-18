93fa864bb3b6ecb156a3430ed1ea4958a22eae98
Kuz Le <k.leshakov@yandex.ru>
2020-05-18 14:03:04 +0300

    (HYD-260) Fix model address (#8)
    * Cluster.address to http_address
    
    * Get_training/production_data change signature

--- hydro_stat/utils.py
+++ hydro_stat/utils.py
@@ -1,11 +1,13 @@
+from typing import Union
+
 import pandas as pd
 import requests
 import s3fs
-from hydrosdk.model import Model
+from hydrosdk.model import Model, ExternalModel
 
 
-def get_training_data(model: Model, s3_endpoint) -> pd.DataFrame:
-    s3_training_data_path = requests.get(f"{model.cluster.address}/monitoring/training_data?modelVersionId={model.id}").json()[0]
+def get_training_data(model: Union[Model, ExternalModel], s3_endpoint) -> pd.DataFrame:
+    s3_training_data_path = requests.get(f"{model.cluster.http_address}/monitoring/training_data?modelVersionId={model.id}").json()[0]
 
     if s3_endpoint:
         fs = s3fs.S3FileSystem(client_kwargs={'endpoint_url': s3_endpoint})
@@ -15,8 +17,8 @@ def get_training_data(model: Model, s3_endpoint) -> pd.DataFrame:
     return training_data
 
 
-def get_production_data(model: Model, size=1000) -> pd.DataFrame:
-    r = requests.get(f'{model.cluster.address}/monitoring/checks/subsample/{model.id}?size={size}')
+def get_production_data(model: Union[Model, ExternalModel], size=1000) -> pd.DataFrame:
+    r = requests.get(f'{model.cluster.http_address}/monitoring/checks/subsample/{model.id}?size={size}')
     if r.status_code != 200:
         raise ValueError("Unable to fetch production data")
     return pd.DataFrame.from_dict(r.json())
