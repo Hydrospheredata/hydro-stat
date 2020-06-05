909484f1840429dbf97b52fb0e5c50581c598f21
Yuriy Gavrilin <snyderino@gmail.com>
2020-06-05 10:03:51 +0300

    fix test messages

--- app/test_messages.py
+++ app/test_messages.py
@@ -7,35 +7,35 @@ def threshold_to_apa_style(t: float):
 
 def mean_test_message(test: StatisticalTest):
     if test.has_changed:
-        test.message = f"Significant change in the mean, p<{threshold_to_apa_style(test.threshold)}"
+        return f"Significant change in the mean, p<{threshold_to_apa_style(test.threshold)}"
     else:
-        test.message = f"No significant change in the mean"
+        return f"No significant change in the mean"
 
 
 def variance_test_message(test: StatisticalTest):
     if test.has_changed:
-        test.message = f"Significant change in the variance, p<{threshold_to_apa_style(test.threshold)}"
+        return f"Significant change in the variance, p<{threshold_to_apa_style(test.threshold)}"
     else:
-        test.message = f"No significant change in the variance"
+        return f"No significant change in the variance"
 
 
 def median_test_message(test: StatisticalTest):
     if test.has_changed:
-        test.message = f"Significant change in the median, p<{threshold_to_apa_style(test.threshold)}"
+        return f"Significant change in the median, p<{threshold_to_apa_style(test.threshold)}"
     else:
-        test.message = f"No significant change in the median"
+        return f"No significant change in the median"
 
 
 def unique_values_test_message(test: StatisticalTest):
     if test.has_changed:
         new_categories = set(test.production_statistic).difference(set(test.training_statistic))
-        test.message = f"There are new categories {new_categories} that were not observed in the training data."
+        return f"There are new categories {new_categories} that were not observed in the training data."
     else:
-        test.message = f"No change"
+        return f"No change"
 
 
 def chi_square_message(test: StatisticalTest):
     if test.has_changed:
-        test.message = f"Production categorical data has different frequencies at p<{threshold_to_apa_style(test.threshold)}"
+        return f"Production categorical data has different frequencies at p<{threshold_to_apa_style(test.threshold)}"
     else:
-        test.message = f"Difference between training and production frequencies are not significant"
+        return f"Difference between training and production frequencies are not significant"
